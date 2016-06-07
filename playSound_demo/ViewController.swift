ddbcda6d81c82e377e01aac7b31248e9368fd57b
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-07 22:28:52 +0800

    PlaySound_01

--- playSound_demo/ViewController.swift
+++ playSound_demo/ViewController.swift
@@ -1,18 +1,22 @@
-//
-//  ViewController.swift
-//  playSound_demo
-//
-//  Created by MikeWu on 2016/6/6.
-//  Copyright © 2016年 practice. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // 設置一個變數，將它指定為AVAudioPlayer的型別
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do {
+            // 設置一個變數來代表音效的儲存位置
+            let filePath = NSBundle.mainBundle().pathForResource("airplane1", ofType: "mp3")
+            // 將指定為AVAudioPlayer型別的變數與要播放的音效做關聯
+            audioPlayer = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: filePath!))
+        } catch {
+            print("Error")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playButton(sender: AnyObject) {
+        audioPlayer.play()
+    }
 
 }
 
